48736b6539c10893354c55ec2681781bf09696dd
Diya-kul <diyadewa05@gmail.com>
2026-09-03 21:27:02 +0530

    feta: optimize app.py file

--- app.py
+++ app.py
@@ -1,45 +1,52 @@
-
-from features import extract_features
 from compare import dtw_distance
 from feedback import generate_feedback
 from predict import predict_accent
+
 import os
 import sys
 
 # user choose a word
-'''
-need to add audio manually
-word = input("Enter word to practice: ").lower()'''
-
 word = 'hello'
 
+# AUDIO PATHS
 user_audio = "Samples/user.wav"
-
 ref_audio= f"data/reference/{word}.wav"
 
-if not os.path.exists(ref_audio):
-    print(f"Sorry! NO reference audio found for '{word}")
+# Check if user audio exists
+if not os.path.isfile(user_audio):
+    print(f"User audio file not found: {user_audio}")
     sys.exit()
 
-#Pronunciation evaluation
+
+# Check if reference audio exists
+if not os.path.isfile(ref_audio):
+    print(f"No reference audio found for '{word}'")
+    sys.exit()
+
+print("Analyzing your pronunciation....\n")
+
+
+# 1. Pronunciation evaluation using DTW
 distance = dtw_distance( user_audio, ref_audio)
 
-# convert DTW distance to score
+# 2. convert DTW distance to score
 score = max(0, 100 - int(distance/4000))
 
-
+# 3. Generate Feedback
 feedback = generate_feedback(distance)
 
-# RESULT 
-print("\n----Accent Coach Result -----")
-print("word:", word)
-print("DTW Distance: ", distance)
-print("Pronunciation score:", score, "/100")
-print("Feedback: ",feedback)
-
-#Accent detection
+# 4. Detect accent
 accent = predict_accent(user_audio)
-print("Predicted Accent:",accent)
+
+
+# Display results
+print("---------- AI ACCENT COACH RESULT ----------")
+print(f"Word: {word}")
+print(f"Predicted Accent: {accent}")
+print(f"DTW Distance: {distance:.2f}")
+print(f"Pronunciation Score: {score}/100")
+print(f"Feedback: {feedback}")
+print("--------------------------------------------")
 
 
 
